0a9c3ca9aeb295477064ce3d2a2b155233c5da40
Neil Hardman <nhardman@uk.ibm.com>
2018-07-04 15:52:35 +0100

    get with Int subscript

--- Sources/TypeDecoder/OrderedDictionary.swift
+++ Sources/TypeDecoder/OrderedDictionary.swift
@@ -54,6 +54,17 @@ public struct OrderedDictionary<K: Hashable, V> {
         }
     }
 
+    /// Return the element value at the numeric index specified.
+    public subscript(index: Int) -> V? {
+        get {
+            if 0 ... self.keys.count - 1 ~= index {
+                let key = self.keys[index]
+                return self.values[key]
+            }
+            return nil
+        }
+    }
+
     /// Read only property that provides a String containing the key:value pairs in the OrderedDictionary.
     public var description: String {
         var result = ""
